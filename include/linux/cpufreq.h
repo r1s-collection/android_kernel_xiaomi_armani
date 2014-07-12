5f8c88992d13b89706633d1a1b738877464cac5a
yank555-lu <yank555.lu@gmail.com>
2014-07-12 16:37:55 +0200

    cpufreq: add yankdemand governor
    -Full stock (JB) ondemand governor with changed default
     tunable values aimed at lower battery consumption
    -Adapt to armani
    
    Change-Id: I2006ae1ea67dd775aa1e2b50c316c9313452055b
    Signed-off-by: khanfrd <khanfrd94@gmail.com>

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -371,6 +371,9 @@ extern struct cpufreq_governor cpufreq_gov_userspace;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_ONDEMAND)
 extern struct cpufreq_governor cpufreq_gov_ondemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_ondemand)
+#elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
+extern struct cpufreq_governor cpufreq_gov_yankdemand;
+#define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
